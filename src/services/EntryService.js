1ebe9607eff26c6cd66e4f8fdd6141eaefcd951c
Jason Blanchard <jas.blanchard@gmail.com>
2016-12-22 19:49:31 -0500

    fixed records

--- src/services/EntryService.js
+++ src/services/EntryService.js
@@ -1,4 +1,5 @@
 import { Schema } from 'mongoose';
+import moment from 'moment';
 
 const LOG_TAG = 'EntryService';
 
@@ -18,11 +19,17 @@ export default class EntryService {
 
   list() {
     return new Promise((resolve, reject) => {
-      this._model.find((error, entries) => {
+      this._model.find((error, records) => {
         if (error) {
           this._logger.error(error, LOG_TAG);
           return reject(error);
         }
+
+        const entries = records.map(record => ({
+          id: record.id,
+          text: record.text,
+          timeCreated: record.timeCreated
+        }));
 
         this._logger.debug({ entries }, LOG_TAG);
         return resolve(entries);
@@ -30,17 +37,24 @@ export default class EntryService {
     })
   }
 
-  create(fields) {
+  create(params) {
+    const fields = Object.assign({}, params, { timeCreated: moment().format() });
+
     this._logger.debug({ fields }, LOG_TAG);
+
     const entry = new this._model(fields);
     return new Promise((resolve, reject) => {
-      entry.save((error, entry) => {
+      entry.save((error, record) => {
         if (error) {
           this._logger.error(error, LOG_TAG);
           return reject(error);
         }
 
+        const { _id: id, text, timeCreated } = record;
+        const entry = { id, text, timeCreated };
+
         this._logger.debug({ entry }, LOG_TAG);
+
         return resolve(entry);
       });
     });
