da8454a8da925356d357dd6bf5e8f8b8cb1e2fd0
Jason Blanchard <jas.blanchard@gmail.com>
2017-01-02 08:36:21 -0500

    refactored server-side service

--- src/services/EntryService.js
+++ src/services/EntryService.js
@@ -4,9 +4,19 @@ import moment from 'moment';
 const LOG_TAG = 'EntryService';
 
 const EntrySchema = new Schema({
+  text: String,
   timeCreated: String,
-  text: String
+  timeUpdated: String
 });
+
+function mapRecordToObject(record) {
+  return {
+    id: record.id,
+    text: record.text,
+    timeCreated: record.timeCreated,
+    timeUpdated: record.timeUpdated
+  }
+}
 
 export default class EntryService {
   constructor(store, logger) {
@@ -19,50 +29,25 @@ export default class EntryService {
 
   list() {
     return this._model.find().then(records => {
-      const entries = records.map(record => ({
-        id: record.id,
-        text: record.text,
-        timeCreated: record.timeCreated
-      }));
-
+      const entries = records.map(mapRecordToObject);
       this._logger.debug({ entries }, LOG_TAG);
+
       return entries;
     })
   }
 
   create(params) {
     const fields = Object.assign({}, params, { timeCreated: moment().format() });
-
     this._logger.debug({ fields }, LOG_TAG);
 
     const entry = new this._model(fields);
-
-    return entry.save().then(record => {
-      const entry = {
-        id: record.id,
-        text: record.text,
-        timeCreated: record.timeCreated
-      }
-
-      this._logger.debug({ entry }, LOG_TAG);
-
-      return entry;
-    });
+    return entry.save().then(mapRecordToObject);
   }
 
   update(id, params) {
-    this._logger.debug({ params }, LOG_TAG);
+    const fields = Object.assign({}, params, { timeUpdated: moment().format() });
+    this._logger.debug({ fields }, LOG_TAG);
 
-    return this._model.findByIdAndUpdate(id, { $set: params }, { new: true }).then(record => {
-      const entry = {
-        id: record.id,
-        text: record.text,
-        timeCreated: record.timeCreated
-      }
-
-      this._logger.debug({ entry }, LOG_TAG);
-
-      return entry;
-    });
+    return this._model.findByIdAndUpdate(id, { $set: fields }, { new: true }).then(mapRecordToObject);
   }
 }
