5c60e2089b66a506aa221bd46486ac7dfc5bd844
Jason Blanchard <jas.blanchard@gmail.com>
2017-01-10 07:57:53 -0500

    fixed redux dev tools

--- client/src/bootstrap/bootstrapStore.js
+++ client/src/bootstrap/bootstrapStore.js
@@ -35,7 +35,7 @@ export default function(registry) {
         new Matcher(routes),
       ),
       applyMiddleware(thunk.withExtraArgument(registry)),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : createStore => createStore,
     ),
   );
 }
