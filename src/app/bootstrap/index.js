d5580884cf8fa45d2511dba746b3c64319bc64fd
Jason Blanchard <jas.blanchard@gmail.com>
2017-02-11 15:14:16 -0500

    renamed app => server

--- src/app/bootstrap/index.js
+++ src/app/bootstrap/index.js
@@ -1,21 +1,21 @@
 import Bottle from 'bottlejs';
 
-import bootstrapApp from './bootstrapApp';
 import bootstrapEntryController from './bootstrapEntryController';
 import bootstrapEntryService from './bootstrapEntryService';
 import bootstrapLogger from './bootstrapLogger';
 import bootstrapLoginController from './bootstrapLoginController';
+import bootstrapServer from './bootstrapServer';
 import bootstrapStore from './bootstrapStore';
 import bootstrapUserService from './bootstrapUserService';
 
 export default function() {
   const bottle = new Bottle();
 
-  bottle.factory('app', bootstrapApp);
   bottle.factory('entryController', bootstrapEntryController);
   bottle.factory('entryService', bootstrapEntryService);
   bottle.factory('logger', bootstrapLogger);
   bottle.factory('loginController', bootstrapLoginController);
+  bottle.factory('server', bootstrapServer);
   bottle.factory('store', bootstrapStore);
   bottle.factory('userService', bootstrapUserService);
 
--- src/app/index.js
+++ src/app/index.js
@@ -6,11 +6,11 @@ environment.config({ silent: true });
 const LOG_TAG = 'entrypoint';
 
 const registry = bootstrap();
-const { logger, app } = registry;
+const { logger, server } = registry;
 
 logger.debug('>>> STARTING <<<', LOG_TAG);
 
 const port = process.env.PORT || 8081;
-app.listen(port, () => {
+server.listen(port, () => {
   logger.debug(`running on port ${port}`, LOG_TAG);
 });
