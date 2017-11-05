2a90ed0f4ae6cd2fc84d1f19a3d7549cf5a318e1
Jason Blanchard <jas.blanchard@gmail.com>
2017-11-05 09:37:13 -0500

    Refactored authenticated route

--- client2/src/Routes.js
+++ client2/src/Routes.js
@@ -7,14 +7,13 @@ import React, { Component } from 'react';
 import { ConnectedLoginPage } from 'src/pages/LoginPage';
 import AuthenticatedRoute from 'src/components/AuthenticatedRoute';
 import ProtectedPage from 'src/pages/ProtectedPage';
-import TokenUtils from 'src/utils/TokenUtils';
 
 export default class Routes extends Component {
   render() {
     return (
       <Router>
         <div>
-          <AuthenticatedRoute exact path="/" accessToken={TokenUtils.getAccessToken()} render={() => <ProtectedPage />} />
+          <AuthenticatedRoute exact path="/" render={() => <ProtectedPage />} />
           <Route path="/login" render={() => <ConnectedLoginPage />} />
         </div>
       </Router>
--- client2/src/components/AuthenticatedRoute.js
+++ client2/src/components/AuthenticatedRoute.js
@@ -1,19 +1,16 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router';
+import TokenUtils from 'src/utils/TokenUtils';
 
 export default class AuthenticatedRoute extends Component {
   static propTypes = {
     render: PropTypes.func.isRequired,
-    accessToken: PropTypes.string,
-  }
-
-  static defaultProps = {
-    accessToken: undefined,
   }
 
   render() {
-    const { render, accessToken, ...rest } = this.props;
+    const { render, ...rest } = this.props;
+    const accessToken = TokenUtils.getAccessToken();
 
     // TODO: Tack on original path to the redirect path in query param.
     return (
