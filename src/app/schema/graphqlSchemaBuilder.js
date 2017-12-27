81bb6485ff370a3ab3b1113860082c92fdffe90c
Jason Blanchard <jas.blanchard@gmail.com>
2017-12-27 14:23:44 -0500

    Added create and update

--- src/app/schema/graphqlSchemaBuilder.js
+++ src/app/schema/graphqlSchemaBuilder.js
@@ -1,15 +1,16 @@
 import {
   graphql,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
   GraphQLID,
+  GraphQLInputObjectType,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
   GraphQLList,
 } from 'graphql';
 
 export default function graphqlSchemaBuilder(entryService, userService) {
   const EntryType = new GraphQLObjectType({
-    name: 'Entry',
+    name: 'EntryType',
     fields: () => ({
       id: { type: GraphQLID },
       text: { type: GraphQLString },
@@ -25,31 +26,73 @@ export default function graphqlSchemaBuilder(entryService, userService) {
   });
 
   const UserType = new GraphQLObjectType({
-    name: 'User',
+    name: 'UserType',
     fields: () => ({
       id: { type: GraphQLID },
       username: { type: GraphQLString },
       email: { type: GraphQLString },
       timeCreated: { type: GraphQLString },
     })
+  });
+
+  const EntriesQueryType = {
+    name: 'EntriesQueryType',
+    type: new GraphQLList(EntryType),
+    resolve(parentValue, args, request) {
+      const userId = '5a400afebf5614778f41f62a';
+      return entryService.list(userId);
+    }
+  };
+
+  const EntryInputType = new GraphQLInputObjectType({
+    name: 'EntryInputType',
+    fields: {
+      text: { type: GraphQLString },
+    }
   })
 
-  const queryType = new GraphQLObjectType({
+  const UpdateEntryMutationQueryType = {
+    name: 'UpdateEntryMutationQueryType',
+    type: EntryType,
+    args: {
+      id: { type: GraphQLID },
+      input: { type: EntryInputType },
+    },
+    resolve(parentValue, args, request) {
+      const userId = '5a400afebf5614778f41f62a';
+      return entryService.update(args.id, userId, args.input);
+    }
+  }
+
+  const CreateEntryMutationQuery = {
+    name: 'CreateEntryMutationQuery',
+    type: EntryType,
+    args: {
+      input: { type: EntryInputType },
+    },
+    resolve(parentValue, args, request) {
+      const userId = '5a400afebf5614778f41f62a';
+      return entryService.create(args.input, userId);
+    }
+  }
+
+  const rootQueryType = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-      entries: {
-        name: 'entries',
-        type: new GraphQLList(EntryType),
-        resolve(parentValue, args, request) {
-          const userId = '5a400afebf5614778f41f62a';
-          // const userId = request.accessTokenPayload.id;
-          return entryService.list(userId);
-        }
-      },
+      entries: EntriesQueryType
     }
   });
 
+  const rootMutationType = new GraphQLObjectType({
+    name: 'RootMutationType',
+    fields: {
+      updateEntry: UpdateEntryMutationQueryType,
+      createEntry: CreateEntryMutationQuery,
+    }
+  })
+
   return new GraphQLSchema({
-    query: queryType
+    query: rootQueryType,
+    mutation: rootMutationType,
   });
 }
