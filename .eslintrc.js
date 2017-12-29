811361d105b979efa0ebdf403fd449fcd92fab5a
Jason Blanchard <jas.blanchard@gmail.com>
2017-12-29 16:52:18 -0500

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,10 @@ module.exports = {
     "parser": "babel-eslint",
     "plugins": [
         "import",
-        "jsx-a11y",
         "react",
     ],
     "env": {
-      "browser": true
+      "node": true
     },
     "rules": {
       "arrow-body-style": 0,
@@ -16,27 +15,11 @@ module.exports = {
       "import/extensions": 0,
       "import/no-extraneous-dependencies": 0,
       "import/no-unresolved": 0,
-      "jsx-a11y/anchor-is-valid": [ "error", {
-        "components": [],
-      }],
-      "jsx-a11y/label-has-for": [ 2, {
-        "required": {
-          "every": [ "id" ]
-        },
-        "allowChildren": false,
-      }],
       "max-len": 0,
       "no-param-reassign": 0,
       "no-underscore-dangle": 0,
       "prefer-destructuring": 0,
       "prefer-template": 0,
-      "react/forbid-prop-types": 0,
-      "react/jsx-filename-extension": 0,
-      "react/no-multi-comp": 0,
-      "react/no-unused-state": 0,
-      "react/prefer-stateless-function": 0,
-      "react/require-default-props": 0,
-      "react/sort-comp": 0,
       "space-before-function-paren": 0,
     }
 };
