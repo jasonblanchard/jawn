9d186548ca8955862697a5c043a424c893a94002
Jason Blanchard <jas.blanchard@gmail.com>
2018-02-13 20:47:27 -0500

    deploy file

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errtrace
+set -o errexit
+
+# NOTE: This needs to happen prior to the trap statements or weird stuff happens
+[ -s "$NVM_DIR/nvm.sh" ] && . $NVM_DIR/nvm.sh # Load nvm
+nvm use
+
+npm install
+npm run build-all-production
+
+docker-compose build app
+docker-compose push app
+
+eval $(docker-machine env linode)
+
+docker stack deploy --compose-file docker-compose.yml jawnapp
