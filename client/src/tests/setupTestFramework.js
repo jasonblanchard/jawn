927b609d2edd8039b458954ec9e8060daf5557d7
Jason Blanchard <jas.blanchard@gmail.com>
2018-12-01 15:37:33 -0500

    Navigator config

--- client/src/tests/setupTestFramework.js
+++ client/src/tests/setupTestFramework.js
@@ -7,3 +7,7 @@ const { window } = jsdom;
 
 global.window = window;
 global.document = window.document;
+global.navigator = {
+  appName: 'Netscape',
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36', // Chrome
+};
