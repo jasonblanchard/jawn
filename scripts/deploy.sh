21b6adfc32c746d3e72f3e850fb9a015d8aa14f5
Jason <jas.blanchard@gmail.com>
2018-12-28 13:43:33 -0500

    Fix deploy script (#8)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,9 @@ set -e
 
 eval $(docker-machine env linode)
 TAG=$1
+IMAGE=jasonblanchard/jawn-app
 
 # TODO: Move to provision.sh
 # docker stack deploy --compose-file docker-compose.yml jawnapp
 
-docker service update --image $TAG jawnapp_app
+docker service update --image "$IMAGE:$TAG" jawnapp_app
