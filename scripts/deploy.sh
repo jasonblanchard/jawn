783aa0d0a1eca9f3723df10d91388d17f7696730
Jason <jas.blanchard@gmail.com>
2018-12-29 10:18:32 -0500

    Use docker stack deploy again (#10)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,7 @@ set -e
 
 eval $(docker-machine env linode)
 TAG=$1
-IMAGE=jasonblanchard/jawn-app
 
-# TODO: Move to provision.sh
-# docker stack deploy --compose-file docker-compose.yml jawnapp
+TAG="${TAG}" docker stack deploy --compose-file docker-compose.prod.yml jawnapp
 
-docker pull "$IMAGE:$TAG"
-docker service update --image "$IMAGE:$TAG" jawnapp_app
+eval $(docker-machine env -u)
