70594bd3599f6c7fb64e2969211d9dcbc9b254e5
Jason Blanchard <jas.blanchard@gmail.com>
2019-01-06 17:51:35 -0500

    Better handling of the registry

--- src/app/bootstrap/index.ts
+++ src/app/bootstrap/index.ts
@@ -1,5 +1,5 @@
-import Bottle from 'bottlejs';
-import { Registry } from './registry';
+import Bottle, { IContainer } from 'bottlejs';
+import { Registry as RegistryInterface } from './registry';
 
 import bootstrapGraphqlService from './bootstrapGraphqlService';
 import bootstrapLogger from './bootstrapLogger';
@@ -8,16 +8,53 @@ import bootstrapServer from './bootstrapServer';
 import bootstrapStore from './bootstrapStore';
 import bootstrapUserService from './bootstrapUserService';
 
+class Registry implements RegistryInterface, IContainer {
+  private container: IContainer
+  $decorator: any // Not using these, just there to make the type checker happy.
+  $register: any
+  $list: any
+  
+  constructor(container: IContainer) {
+    this.container = container;
+    this.$decorator = container.$decorator;
+    this.$register = container.$register;
+    this.$list = container.$list; 
+  }
+
+  get graphqlService() {
+    return this.container.graphqlService;
+  }
+
+  get logger() {
+    return this.container.logger;
+  }
+
+  get loginController() {
+    return this.container.loginController;
+  }
+
+  get server() {
+    return this.container.server;
+  }
+
+  get store() {
+    return this.container.store;
+  }
+
+  get userService() {
+    return this.container.userService;
+  }
+}
+
 export default function(): Registry {
   const bottle = new Bottle();
 
-  bottle.factory('graphqlService', bootstrapGraphqlService);
+  bottle.factory('graphqlService', (registry: Registry) => bootstrapGraphqlService(registry));
   bottle.factory('logger', bootstrapLogger);
-  bottle.factory('loginController', bootstrapLoginController);
-  bottle.factory('server', bootstrapServer);
+  bottle.factory('loginController', (registry: Registry) => bootstrapLoginController(registry));
+  bottle.factory('server', (registry: Registry) => bootstrapServer(registry));
   bottle.factory('store', bootstrapStore);
-  bottle.factory('userService', bootstrapUserService);
+  bottle.factory('userService', (registry: Registry) => bootstrapUserService(registry));
 
-  // TODO: Figure out a better way to do this, this is kind of dumb.
-  return <Registry>bottle.container;
+  return new Registry(bottle.container);
 }
--- src/app/bootstrap/registry.d.ts
+++ src/app/bootstrap/registry.d.ts
@@ -1,14 +1,15 @@
-import { IContainer } from 'bottlejs';
 import { MongoStore } from 'app/services/MongoService';
+import { Application } from 'express';
 import UserService from 'app/services/UserService';
 import GraphqlService from 'app/services/GraphqlService';
 import LoggerService, { LogLevelDesc } from 'app/services/LoggerService';
 import LoginController from 'app/controllers/LoginController';
 
-export interface Registry extends IContainer {
+export interface Registry {
     userService: UserService
     graphqlService: GraphqlService
     logger: LoggerService
     loginController: LoginController
     store: MongoStore
+    server: Application
 }
