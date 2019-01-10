93ff074d20358381eed431aac968d706ff75ea5e
Jason Blanchard <jas.blanchard@gmail.com>
2019-01-10 08:04:11 -0500

    Fix error

--- src/app/bootstrap/registry.d.ts
+++ src/app/bootstrap/registry.d.ts
@@ -2,7 +2,7 @@ import { MongoStore } from 'app/services/MongoService';
 import { Application } from 'express';
 import UserService from 'app/services/UserService';
 import GraphqlService from 'app/services/GraphqlService';
-import LoggerService, { LogLevelDesc } from 'app/services/LoggerService';
+import LoggerService from 'app/services/LoggerService';
 import LoginController from 'app/controllers/LoginController';
 
 export interface Registry {
