4fbe91bfdfb25f38e4720a720a8f01420a7c8ce2
Jason Blanchard <jas.blanchard@gmail.com>
2019-07-07 15:44:44 -0400

    Moved smoke tests out of workspaces project

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -26,35 +26,11 @@ import jwt from 'jsonwebtoken';
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('createAccessToken', id => {
-  const secret = '$2a$10$HuxYWte8dvZJM9UHBMEFZO'; // TODO: Figure out how to share with docker-compose.ci.tml
-  return new Cypress.Promise(resolve => {
-    jwt.sign({ id }, secret, {}, (error, token) => {
-      if (error) return null;
-      resolve(token);
-    });
-  });
-});
-
 Cypress.Commands.add('login', () => {
-  const context = {};
-
-  return cy.task('db:create:user', {
-    type: 'user',
-    fields: {
-      username: 'test',
-      email: 'test@example.com',
-      password: 'realtestpass',
-    },
-  }).then(user => {
-    context.user = user;
-    return cy.createAccessToken(user.id);
-  }).then(token => {
-    return cy.setCookie('token', token);
-  })
-    .then(() => {
-      return context.user;
-    });
+  cy.visit('/auth/login');
+  cy.get('input[name=username]').type('test');
+  cy.get('input[name=password]').type('testpass');
+  cy.contains('login').click();
 });
 
 Cypress.Commands.overwrite('injectAxe', (orig) => {
